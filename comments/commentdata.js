ad943673f83661b9da6d9ebe1abd82dadeb64a00
Dan Callahan <dan.callahan@gmail.com>
2014-04-05 13:59:09 -0500

    Initial commit

---
+++ comments/commentdata.js
@@ -0,0 +1,13 @@
+var COMMENTS = [{
+ author: "Alice",
+ body: "Cras tristique velit faucibus metus ornare, ut dictum dolor viverra. In mattis tincidunt nulla ac imperdiet. Integer malesuada, lectus sagittis ultricies sagittis, sapien metus feugiat odio, sit amet dapibus lorem urna sed mauris."
+}, {
+  author: "Bob",
+  body: "Curabitur scelerisque lectus leo, et euismod nisl faucibus sit amet. Integer odio lectus, imperdiet at dolor eu, mattis mollis urna."
+}, {
+  author: "Carol",
+  body: "Quisque faucibus est porttitor sem mollis sodales. In hac habitasse platea dictumst."
+}, {
+  author: "Dave",
+  body: "Proin lectus justo, blandit et ante quis, condimentum viverra sapien."
+}];
